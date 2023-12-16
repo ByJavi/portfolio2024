72f76166df133bec94e4a7c04ec43e68cf8fce62
ByJavi <ledesmajaver2019@gmail.com>
2023-12-16 01:27:58 -0300

    [Add] Getfrom.io | prox animation send email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,37 @@
+import { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+
 import CarouselText from './CarouselText/CarouselText'
+import EmailSend from './EmailSend'
 
 function Contact() {
+  const [result, setResult] = useState(true)
+
+  const formulario = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    emailjs
+      .sendForm(
+        'service_kffhrq6',
+        'template_bk1u4y1',
+        formulario.current,
+        '1J-6Rhph15xqABoX3'
+      )
+      .then(
+        (res) => {
+          setResult(res)
+          event.target.reset()
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    setTimeout(() => {
+      setResult(false)
+    }, 5000)
+  }
+
   return (
     <section className="my-40">
       <div className="my-10 py-10">
@@ -9,11 +40,10 @@ function Contact() {
       <article className="max-w-5xl w-11/12 mx-auto">
         <div className="flex justify-center items-center">
           <form
-            action=""
-            method="POST"
+            ref={formulario}
+            onSubmit={handleSubmit}
             className="flex flex-col w-full md:w-7/12"
           >
-            <title>Contactame</title>
             <input
               type="text"
               name="name"
@@ -28,22 +58,24 @@ function Contact() {
             ></input>
 
             <textarea
-              name="Mensaje"
+              type="text"
+              name="mensaje"
               placeholder="Comunica tu idea y hagámosla realidad"
               rows="10"
               className="p-2 mb-4 border-2 rounded-sm dark:bg-transparent border-stone-950 dark:border-violet-700 focus:outline-none focus:bg-stone-300 dark:focus:bg-stone-700"
             ></textarea>
 
             <button
-              type="button"
+              type="submit"
               className="relative inline-block px-8 py-3 font-medium group"
             >
               <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0 dark:bg-violet-700"></span>
               <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black dark:bg-violet-300 dark:group-hover:bg-violet-700"></span>
               <span className="relative text-black group-hover:text-white">
-                Trabajo Conmigo
+                Trabajemos Juntos
               </span>
             </button>
+            <div>{result ? <EmailSend /> : null}</div>
           </form>
         </div>
       </article>
