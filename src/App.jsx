3d860a9cda16f7bc32615207f01444d27ac5e496
ByJavi <ledesmajaver2019@gmail.com>
2023-12-16 07:15:56 -0300

    [Fixture] Add ScrollY UP

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,55 @@
 import { useEffect, useState } from 'react'
-
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Portfolio from './components/Portfolio'
 import Resume from './components/Resume'
 import Timeline from './components/Timeline'
-
 import { sun, moon } from './utils/constants'
 import ScrollUp from './components/ScrollUp'
 
 function App() {
-  // Inicializar el tema desde el localStorage o establecer 'dark' como valor predeterminado
   const storedTheme = localStorage.getItem('theme')
   const [theme, setTheme] = useState(storedTheme || 'dark')
-  // const [isVisible, setIsVisible] = useState(true)
+  const [scrollY, setScrollY] = useState(0)
 
-  // Usar un useEffect para configurar el tema inicial basado en la configuración del sistema
   useEffect(() => {
     const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
       .matches
       ? 'dark'
       : 'light'
 
-    // Establecer el tema inicial solo si no hay un tema almacenado en el localStorage
     if (!storedTheme) {
       setTheme(systemTheme)
     }
   }, [storedTheme])
 
-  // Usar otro useEffect para aplicar el tema y guardarlo en el localStorage
   useEffect(() => {
     document.documentElement.classList.toggle('dark', theme === 'dark')
     localStorage.setItem('theme', theme)
   }, [theme])
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   const handleThemeChange = () => {
     setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'))
   }
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrollY = window.scrollY || document.documentElement.scrollTop
-  //     setIsVisible(scrollY > window.innerHeight)
-  //   }
-
-  //   window.addEventListener('scroll', handleScroll)
-
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll)
-  //   }
-  // }, [])
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
 
   return (
     <>
@@ -62,7 +62,8 @@ function App() {
       >
         {theme === 'dark' ? sun : moon}
       </button>
-      <main className="bg-white dark:bg-stone-950 text-stone-900 dark:text-stone-300 min-h-screen font-inter ">
+
+      <main className="bg-white dark:bg-stone-950 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
         <div className="">
           <Resume />
           <Portfolio />
@@ -70,7 +71,9 @@ function App() {
           <Contact />
           <Footer />
         </div>
-        <ScrollUp theme={theme} />
+        {scrollY > 100 && (
+          <ScrollUp theme={theme} handleScrollToTop={handleScrollToTop} />
+        )}
       </main>
     </>
   )
