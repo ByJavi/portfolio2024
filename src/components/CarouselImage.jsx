b178b62f093f3e272c7808b1708c3a72a5e3cc94
ByJavi <ledesmajaver2019@gmail.com>
2023-12-28 16:33:57 -0300

    [Fixture] Carousel Image Scroll

---
+++ src/components/CarouselImage.jsx
@@ -0,0 +1,53 @@
+import { motion, useTransform, useScroll } from 'framer-motion'
+import { useRef } from 'react'
+
+import PortfolioCard from './PortfolioCard'
+
+const CarouselImage = () => {
+  const targetRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: targetRef
+  })
+  const x = useTransform(scrollYProgress, [0, 1], ['1%', '-95%'])
+
+  const CardExample = [
+    {
+      url: './wallpaper.png',
+      title: 'example',
+      id: 1
+    },
+    {
+      url: './wallpaper.png',
+      title: 'example',
+      id: 2
+    },
+    {
+      url: './wallpaper.png',
+      title: 'example',
+      id: 3
+    },
+    {
+      url: './wallpaper.png',
+      title: 'example',
+      id: 4
+    },
+    {
+      url: './wallpaper.png',
+      title: 'example',
+      id: 5
+    }
+  ]
+  return (
+    <section ref={targetRef} className="relative  h-[300vh]">
+      <div className="sticky top-0 h-screen flex items-center overflow-hidden">
+        <motion.div className="flex gap-4" style={{ x }}>
+          {CardExample.map((card) => {
+            return <PortfolioCard card={card} key={card.id} />
+          })}
+        </motion.div>
+      </div>
+    </section>
+  )
+}
+
+export default CarouselImage
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,28 +1,19 @@
-import LazyLoad from 'react-lazyload'
 import { Link } from 'react-router-dom'
 import Fade from 'react-reveal/Fade'
 
 import Button from './Button'
 import CarouselText from './CarouselText'
-import PortfolioCard from './PortfolioCard'
+import CarouselImage from './CarouselImage'
 
 // Ajusta la ruta según la ubicación real
 function Portfolio() {
   return (
     <section name="portfolio" className="my-10">
-      <div className="my-10 py-10">
-        <CarouselText texts="PORTFOLIO" />
-      </div>
       <Fade bottom>
-        <div className="flex flex-col gap-8">
-          <LazyLoad height={200} offset={200}>
-            <PortfolioCard />
-          </LazyLoad>
-        </div>
-        <div
-          className="flex items-center justify-center m-10 p-5"
-          data-aos="slide-up"
-        >
+        <CarouselText texts="PORTFOLIO" />
+
+        <CarouselImage />
+        <div className="flex items-center justify-center m-10 p-5">
           <Link to="/works">
             <Button texts="Mas Proyectos" />
           </Link>
